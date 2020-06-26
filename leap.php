b73b3a1163be76c785073232c97d419eda5b507d
Tatu360 <tatukassim66@gmail.com>
2020-06-26 15:02:27 +0300

    Add files via upload

---
+++ leap.php
@@ -0,0 +1,12 @@
+<?php
+function LeapYear($year) {
+    echo" Its leap Year!";
+}   
+    if (year % 4==0)
+       print "True";
+    elseif (year % 100==0)
+        print "False";
+    else (year % 400==0)
+    return (true)
+
+        ?>
